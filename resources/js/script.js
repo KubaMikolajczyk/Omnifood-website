61bc9af2b116b07d30b2f5f04f488fea76bf88c2
KubaMikolajczyk <qba600@gmail.com>
2019-01-26 16:28:09 +0100

    implement mobile nav panel

--- resources/js/script.js
+++ resources/js/script.js
@@ -62,4 +62,34 @@ $(document).ready(function() {
         offset: '50%'
     });
     
+    /* Mobile nav Animation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon= $('.js--nav-icon ion-icon');
+        nav.slideToggle(200);
+        
+        if (icon.attr("name") === 'menu') {
+                    icon.attr("name", 'close');
+                } else {
+                    icon.attr("name", 'menu');
+                }
+    /* Window resizing and nav display */
+        $(window).resize(function(){
+
+            if ($(window).width() > 767){
+                nav.css("display", "block");
+            } 
+            else {
+                nav.css("display", "none");
+                icon.attr("name", 'menu')
+            }
+        });
+    });
+        
+        
+       
+        
+        
+    
+    
 });
